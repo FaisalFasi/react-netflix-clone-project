59c02e9d106a4c13ec79b8bbd7ffc0a90f1234dc
faisalrehman <faisalfasi18@gmail.com>
2023-07-27 00:26:20 +0200

    bg rendering fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth } from "../utilities/firebase-configs";
 import { useState } from "react";
 import Header from "../components/Header";
-// import "./signup.css";
+import BackgroundImage from "../components/BackgroundImage";
 
 import { useNavigate } from "react-router-dom";
 export default function Signup() {
@@ -54,27 +54,24 @@ export default function Signup() {
   };
 
   return (
-    <div
-      className=" min-h-screen  bg-my-dynamic-image bg-cover"
-      style={{ backgroundImage: `url(${"../src/assets/login.jpg"})` }}
-    >
-      {/* <BackgroundImage /> */}
-      <div className="min-h-screen w-full bg-black bg-opacity-60 bg-cover ">
-        <Header login />
-        <div className="md:pt-20">
-          <div className="flex flex-col items-center text-center mt-20 px-8 text-white">
+    <>
+      <div className=" min-h-screen relative">
+        <BackgroundImage />
+        <div className="absolute  top-0 left-0 h-full w-full  bg-black bg-opacity-60">
+          <Header login />
+          <div className=" flex flex-col gap-4  pt-10 sm:pt-[10%] md:pt-[15%] lg:pt-[20%]  mx-8 md:mx-20 justify-center items-center text-center text-white">
             <h1 className="text-2xl md:text-2xl lg:text-4xl font-extrabold">
               Unlimited movies, TV shows and more
             </h1>
-            <p className="mt-2 text-md md:text-lg font-normal">
+            <p className=" text-md md:text-lg font-normal">
               Watch anywhere. Cancel anytime.
             </p>
-            <p className="mt-6 text-sm md:text-md font-normal">
+            <p className="mt-2 text-sm md:text-md font-normal">
               Ready to watch? Enter your email to create or restart membership
             </p>
           </div>
           <form onSubmit={handleSignIn} method="post">
-            <div className="flex flex-col items-center justify-center md:flex-row gap-2 mt-10 px-8 text-black">
+            <div className="w-full flex flex-col md:flex-row items-center justify-center  gap-2 mt-8 px-8 text-black">
               <input
                 type="email"
                 placeholder="Email Address"
@@ -85,13 +82,13 @@ export default function Signup() {
                 required
               />
               {showPassword ? (
-                <div className="w-full justify-center">
+                <div className="w-[80%] md:w-[50%] flex flex-col md:flex-row gap-2 justify-center items-center text-center">
                   <input
                     type="password"
                     placeholder="Password"
                     name="password"
                     minLength={6}
-                    className="passwordInput border border-black text-black rounded px-2 py-4"
+                    className="w-full md:w-[80%] passwordInput border border-black text-black rounded px-2 py-4"
                     value={formValues.password}
                     onChange={(e) =>
                       setFormValues({
@@ -103,9 +100,9 @@ export default function Signup() {
                     autoComplete="username"
                   />
                   <button
-                    className="text-center bg-red-600 text-white mt-2 rounded px-6 py-4"
-                    // type="submit"
-                    // onClick={handleSignIn}
+                    className="w-full md:w-[30%] text-center bg-red-600 text-white rounded py-4 px-2  "
+                    type="submit"
+                    onClick={handleSignIn}
                   >
                     Sign Up
                   </button>
@@ -124,19 +121,6 @@ export default function Signup() {
           </form>
         </div>
       </div>
-    </div>
+    </>
   );
-}
-{
-  /* <div className="flex flex-col gap-5 text-white text-center">
-<h1 className="xl:text-7xl lg:text-5xl md:text-3xl sm:text-xs font-bold">
-  Unlimited movies, TV shows and more
-</h1>
-<h4 className="xl:text-5xl lg:text-3xl md:text-xl sm:text-xs font-semibold">
-  Watch anywhere. Cancel anytime.
-</h4>
-<h6 className="xl:text-lg lg:text-md md:text-sm sm:text-xs font-semibold">
-  Ready to watch? Enter your email to create or restart membership
-</h6>
-</div> */
 }
